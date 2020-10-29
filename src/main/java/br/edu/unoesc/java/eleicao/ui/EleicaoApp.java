c308ef052185e240e4fd06821becf7a9f717cf97
Alex de Carvalho <leco_123carvalho@hotmail.com>
2020-10-28 22:55:42 -0300

    Configurado e Testado

---
+++ src/main/java/br/edu/unoesc/java/eleicao/ui/EleicaoApp.java
@@ -0,0 +1,35 @@
+package br.edu.unoesc.java.eleicao.ui;
+
+import java.util.List;
+
+import br.edu.unoesc.java.eleicao.dao.impl.jpa.PartidoDAOJpa;
+import br.edu.unoesc.java.eleicao.model.Partido;
+
+/**
+ * Executa a aplicação da Eleição
+ * 
+ * @author Roberson Alves
+ *
+ */
+public class EleicaoApp {
+	public static void main(String[] args) {
+		PartidoDAOJpa partidoDAOJpa = new PartidoDAOJpa();
+		
+		// cadastrar um novo partido
+		Partido p = new Partido();
+		p.setNome("Partido da Pós Java");
+		p.setSigla("PPJ");
+		partidoDAOJpa.salvar(p);
+		
+		// excluir
+		p = partidoDAOJpa.get(6);
+		partidoDAOJpa.excluir(p);
+		
+		// listar todos os partidos
+		List<Partido> partidos = partidoDAOJpa.listar();
+		for (Partido partido : partidos) {
+			System.out.println(partido.getNome() + " - "
+					+ partido.getSigla());
+		}
+	}
+}
